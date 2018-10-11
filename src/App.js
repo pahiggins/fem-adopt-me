7edc996dfb1ca836e4b7a587cc8a58f5b25937a2
Peter A. Higgins <pahiggins@cenabo.com>
2018-10-11 15:38:40 +0100

    Refactor header into it's own NavBar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import pf from "petfinder-client";
 import { Provider } from "./SearchContext";
 import Details from "./Details";
 import Results from "./Results";
 import SearchParams from "./SearchParams";
+import NavBar from "./NavBar";
 
 const petfinder = pf({
   key: process.env.API_KEY,
@@ -67,14 +68,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <header>
-          <Link to="/">Adopt me!</Link>
-          <Link to="/search-params">
-            <span aria-label="search" role="img">
-              🔍
-            </span>
-          </Link>
-        </header>
+        <NavBar />
         <Provider value={this.state}>
           <Router>
             <Results path="/" />
